11a1a12a07b707211e8579801a116e9ddf8fc148
KevinChiguano <kevin_chili1998@hotmail.com>
2022-05-28 11:58:11 -0500

    Actualizar

--- src/main/java/com/uce/edu/demo/universidad/Materia.java
+++ src/main/java/com/uce/edu/demo/universidad/Materia.java
@@ -2,4 +2,33 @@ package com.uce.edu.demo.universidad;
 
 public class Materia {
 
+	private String nombre;
+	private String semestre;
+	private String aula;
+
+	// SET y GET
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(String semestre) {
+		this.semestre = semestre;
+	}
+
+	public String getAula() {
+		return aula;
+	}
+
+	public void setAula(String aula) {
+		this.aula = aula;
+	}
+
 }
